3d53ca513f0333fd1ea60fceccf3502ab4d176ef
Rui <rui19981120.sns@gmail.com>
2023-09-23 08:03:24 +0900

    Add
    init

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+	env: {
+		node: true,
+	},
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {},
+	globals: {
+		module: 'readonly',
+		__dirname: 'readonly',
+	},
+};
